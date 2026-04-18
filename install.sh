bb03734349a1ed67b17a6203439ce2efc697d54e
OpsCharlie <verstraete.carl@gmail.com>
2026-04-18 16:59:07 +0200

    fix(install): expand GNOME_PKGS as array in pacstrap
    Refactor GNOME_PKGS from a string to an array for better readability
    and correct expansion in pacstrap. This ensures all GNOME packages are
    properly installed during system setup.

--- install.sh
+++ install.sh
@@ -121,12 +121,27 @@ gnome gdm \
 pipewire pipewire-pulse pipewire-jack wireplumber \
 btrfs-progs snapper grub-btrfs $UCODE tlp"
 
-GNOME_PKGS="baobab decibels epiphany gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager/
-  gnome-connections gnome-console gnome-contacts gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs/
-  gnome-maps gnome-menus gnome-music gnome-remote-desktop gnome-session gnome-settings-daemon gnome-shell gnome-software/
-  gnome-system-monitor gnome-text-editor gnome-tour gnome-user-docs gnome-user-share gnome-weather grilo-plugins gst-thumbnailers gvfs/
-  gvfs-afc gvfs-dnssd gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd loupe malcontent nautilus/
-  orca papers rygel showtime simple-scan snapshot sushi tecla xdg-desktop-portal-gnome xdg-user-dirs-gtk yelp"
+GNOME_PKGS=(
+  baobab decibels epiphany gnome-backgrounds
+  gnome-calculator gnome-calendar gnome-characters
+  gnome-clocks gnome-color-manager
+  gnome-connections gnome-console gnome-contacts
+  gnome-control-center gnome-disk-utility
+  gnome-font-viewer gnome-keyring gnome-logs
+  gnome-maps gnome-menus gnome-music
+  gnome-remote-desktop gnome-session
+  gnome-settings-daemon gnome-shell
+  gnome-software gnome-system-monitor
+  gnome-text-editor gnome-tour
+  gnome-user-docs gnome-user-share
+  gnome-weather grilo-plugins gst-thumbnailers
+  gvfs gvfs-afc gvfs-dnssd gvfs-goa gvfs-gphoto2
+  gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd
+  loupe malcontent nautilus orca papers rygel showtime
+  simple-scan snapshot sushi tecla
+  xdg-desktop-portal-gnome xdg-user-dirs-gtk yelp
+)
+
 
 EXTRA_PKGS=""
 
@@ -137,7 +152,7 @@ fi
 # -----------------------------
 # Install system
 # -----------------------------
-pacstrap /mnt $BASE_PKGS $EXTRA_PKGS $GPU_PKGS $GNOME_PKGS
+pacstrap /mnt $BASE_PKGS $EXTRA_PKGS $GPU_PKGS "${GNOME_PKGS[@]}"
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
