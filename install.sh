a7bb3aad25b2509ab1062caacc760d05c57d5281
OpsCharlie <verstraete.carl@gmail.com>
2026-04-18 16:48:00 +0200

    feat(install): add full GNOME package group to install
    - Define GNOME_PKGS variable with comprehensive GNOME packages
    - Include GNOME_PKGS in pacstrap for complete desktop setup
    - Replace VM package with qemu-guest-agent for virtual machines

--- install.sh
+++ install.sh
@@ -121,16 +121,23 @@ gnome gdm \
 pipewire pipewire-pulse pipewire-jack wireplumber \
 btrfs-progs snapper grub-btrfs $UCODE tlp"
 
+GNOME_PKGS="baobab decibels epiphany gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager/
+  gnome-connections gnome-console gnome-contacts gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs/
+  gnome-maps gnome-menus gnome-music gnome-remote-desktop gnome-session gnome-settings-daemon gnome-shell gnome-software/
+  gnome-system-monitor gnome-text-editor gnome-tour gnome-user-docs gnome-user-share gnome-weather grilo-plugins gst-thumbnailers gvfs/
+  gvfs-afc gvfs-dnssd gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd loupe malcontent nautilus/
+  orca papers rygel showtime simple-scan snapshot sushi tecla xdg-desktop-portal-gnome xdg-user-dirs-gtk yelp"
+
 EXTRA_PKGS=""
 
 if [ "$SYSTEM_TYPE" = "vm" ]; then
-    EXTRA_PKGS="virtio-drivers"
+    EXTRA_PKGS="qemu-guest-agent"
 fi
 
 # -----------------------------
 # Install system
 # -----------------------------
-pacstrap /mnt $BASE_PKGS $EXTRA_PKGS $GPU_PKGS
+pacstrap /mnt $BASE_PKGS $EXTRA_PKGS $GPU_PKGS $GNOME_PKGS
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
