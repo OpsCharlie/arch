d29045c701469b7b63e148af7af9b7f2e93d4903
CAVE <cave@sofico.be>
2026-04-27 15:25:03 +0200

    fix(install): move grub-mkconfig after snapper setup
    Relocate the grub-mkconfig command to run after snapper configuration.
    This ensures that GRUB is aware of any changes made by snapper,
    improving bootloader consistency with the current system state.

--- install.sh
+++ install.sh
@@ -172,7 +172,6 @@ mkinitcpio -P
 sed -i "s|GRUB_CMDLINE_LINUX=\"\"|GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$UUID:cryptroot root=/dev/mapper/cryptroot\"|" /etc/default/grub
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
 
 # users
 useradd -m -G wheel "$USERNAME"
@@ -216,6 +215,8 @@ snapper --no-dbus create \
     --read-write \
     --cleanup-algorithm number \
     --description "initial install"
+
+grub-mkconfig -o /boot/grub/grub.cfg
 
 # -----------------------------
 # Flatpak
